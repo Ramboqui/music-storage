859a55075b6ab235b889083c6311ad85d3d4dd92
Matheus Custodio <eu.258963@hotmail.com>
2022-08-05 17:47:35 -0300

    feat: import primeng and styling menubar

--- music-storage-frontend/src/app/app.component.ts
+++ music-storage-frontend/src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'music-storage-frontend';
+
+  items: MenuItem[] = [
+    { label: 'Home' },
+    { label: 'test1' },
+    { label: 'test2' },
+    { label: 'test3' },
+  ];
 }
--- music-storage-frontend/src/app/app.module.ts
+++ music-storage-frontend/src/app/app.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MenubarModule } from 'primeng/menubar';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [AppRoutingModule, BrowserModule, MenubarModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
