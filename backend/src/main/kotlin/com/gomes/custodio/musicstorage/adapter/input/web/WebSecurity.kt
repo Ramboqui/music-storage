724a5d5e2b4f8ba11b909da45e38a51cb5fc4437
DK-Guilherme <guilhermegsouza520@gmail.com>
2022-08-12 09:51:27 -0300

    Just remove a line

--- backend/src/main/kotlin/com/gomes/custodio/musicstorage/adapter/input/web/WebSecurity.kt
+++ backend/src/main/kotlin/com/gomes/custodio/musicstorage/adapter/input/web/WebSecurity.kt
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.core.annotation.Order
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.web.servlet.invoke
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.web.cors.CorsConfiguration
